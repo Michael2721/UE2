04805d19367dd54f072e8e552a271ce6efa4b62c
1810738077@stud.fh-kufstein.ac.at <1810738077@stud.fh-kufstein.ac.at>
2019-06-01 14:28:58 +0200

    btn animation

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,24 @@
-var btns = document.querySelectorAll('.button');
+var btns = document.querySelectorAll('#buttons button');
+var startbtn = document.querySelector('#start');
+
 
 btns.forEach(function (btn) {
     btn.addEventListener('click', () => {
         console.log("click "+btn.id);
+        flashitem(btn);
     });
 });
 
+
+
+startbtn.addEventListener('click', () => {
+    console.log("click "+startbtn.id);
+});
+
+//btn animation
+function flashitem(item) {
+    item.classList.add('flash');
+    setTimeout(function () {
+        item.classList.remove('flash');
+    }, 150);
+}
