d7700729e9cc22850149de06e88bf89e364ed76c
1810738077@stud.fh-kufstein.ac.at <1810738077@stud.fh-kufstein.ac.at>
2019-06-01 13:02:33 +0200

    change

--- docs/app.js
+++ docs/app.js
@@ -2,30 +2,7 @@ var btns = document.querySelectorAll('.button');
 
 btns.forEach(function (btn) {
     btn.addEventListener('click', () => {
-        console.log("click");
+        console.log("click "+btn.id);
     });
 });
 
-/*
-document.querySelector('#yellow').addEventListener('click', () =>{
-    console.log("clicky");
-});
-
-document.querySelector('#red').addEventListener('click', () =>{
-    console.log("clickr");
-});
-
-document.querySelector('#blue').addEventListener('click', () =>{
-    console.log("clicbk");
-});
-
-document.querySelector('#green').addEventListener('click', () =>{
-    console.log("clickg");
-});
-/*
-document.addEventListener("click", function(){
-    document.getElementById("yellow").innerHTML = "yellow";
-    document.getElementById("red").innerHTML = "red";
-});
-
-*/
