93f673ba375a12a65266da0292ef38aeceff1a1f
1810738077@stud.fh-kufstein.ac.at <1810738077@stud.fh-kufstein.ac.at>
2019-06-02 13:48:53 +0200

    serviceworker fertig

--- docs/app.js
+++ docs/app.js
@@ -8,14 +8,7 @@
     
 
 
-    // setup levels
-    // var levels = [
-    //     [0, 1, 3, 2],
-    //     [0, 0, 1, 3, 1],
-    //     [0, 1, 3, 2, 3, 1, 3],
-    //     [1, 3, 3, 1, 0, 2],
-    //     [0, 3, 0, 1, 2, 1, 0]
-    // ];
+   
     var lvl = [Math.floor(Math.random()*4)];
     var levels = [lvl];
     var levelIdx = 0;
